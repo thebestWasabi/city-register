5840e01389107e6c4a0996a64cac9d8247f9d45e
thebestWasabi <openpw@icloud.com>
2022-10-31 23:47:15 +0300

    0.01.3

---
+++ src/test/java/edu/net/SimpleSocket.java
@@ -0,0 +1,30 @@
+package edu.net;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SimpleSocket {
+
+    @Test
+    public void simpleSocket() throws IOException {
+        Socket socket = new Socket("localhost", 8080);
+
+        InputStream inputStream = socket.getInputStream();
+        OutputStream outputStream = socket.getOutputStream();
+
+        String command = "GET / HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
+        outputStream.write(command.getBytes());
+        outputStream.flush();
+
+        int i = 0;
+        while ((i = inputStream.read()) != -1) {
+            System.out.print((char) i);
+        }
+
+        socket.close();
+    }
+}
